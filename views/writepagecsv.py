62a608c9f0c351c4e9de71c6445c0a2e0a51695d
David Perelroizen <david.perelroizen@gmail.com>
2021-12-08 17:41:50 +0100

    Added a function that will write the CSV file for a set of pages

---
+++ views/writepagecsv.py
@@ -0,0 +1,27 @@
+from controllers.scrappage import scrapPage
+import csv
+
+csv_header = [
+    "product_page_url",
+    "universal_product_code (upc)",
+    "title",
+    "price_including_tax",
+    "price_excluding_tax",
+    "number_available",
+    "product_description",
+    "category",
+    "review_rating",
+    "image_url"
+]
+
+
+def writeCsvFile(header, output_file_name, pages):
+    """Create a CSV file where it will write the result of the page scrapping for a given list of pages"""
+    with open(str(output_file_name) + '.csv', 'w') as outf:
+        writer = csv.writer(outf)
+        writer.writerow(header)
+        for page in pages:
+            scrapping_result = scrapPage(page)
+            writer.writerow(scrapping_result)
+        return outf
+
