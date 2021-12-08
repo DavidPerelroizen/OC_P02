0140a4b46148b480b21e2f3757ebfc3dbe932a72
David Perelroizen <david.perelroizen@gmail.com>
2021-12-08 11:35:57 +0100

    SoupPage function finalized

--- controllers/scrappage.py
+++ controllers/scrappage.py
@@ -3,11 +3,15 @@ import requests
 
 
 def soupPage(url, html_element):
-    """This function extracts a list of specified html_elements from a specified web page"""
+    """This function extracts a list of specified html_elements from a specified web page and returns that list"""
     response = requests.get(url)
     if response.ok:
+        element_list = []
         soup = BeautifulSoup(response.text, features="html.parser")
-        element_list = soup.findAll(html_element)
+        element_extract = soup.findAll(html_element)
+        for i in element_extract:
+            element_list.append(i.text)
         return element_list
+
 
 
