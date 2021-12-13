ab7cbbdbc15abaf6f5e3242b137d9f05ba461a69
David Perelroizen <david.perelroizen@gmail.com>
2021-12-13 11:31:32 +0100

    Cleaned the main file

--- main.py
+++ main.py
@@ -1,10 +1,7 @@
 from views.writepagecsv import csv_header
 from controllers.runfullscrap import fullscrap
 
-url_category = "https://books.toscrape.com/catalogue/category/books/sequential-art_5/index.html"
 url_website = "https://books.toscrape.com"
-url_product_page = 'https://books.toscrape.com/catalogue/category/books/travel_2/index.html'
-
 
 fullscrap(url_website, csv_header)
 
